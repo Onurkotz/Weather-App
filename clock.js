b97fab95c905afb7d12d3a4bf7003578b8c5abac
onur koc <onurkottz@gmail.com>
2022-03-07 16:53:59 +0300

    details

--- clock.js
+++ clock.js
@@ -17,7 +17,7 @@ function clock(){
         "Cumartesi"
     ]
 
-    hourDom.innerHTML = getClock + "   " + (days[new Date().getDay()]);
+    hourDom.innerHTML = "Saat " + getClock + "   " + "günlerden " (days[new Date().getDay()]);
 
     setTimeout(clock, 1000);
 }
